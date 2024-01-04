b7a15f7db35b8db9a20dde59b1705bba1a6cc252
Nikita Popov <npopov@redhat.com>
2024-01-04 16:28:48 +0100

    Add 20240103 triage results

--- update.py
+++ update.py
@@ -421,7 +421,7 @@ comparisons = [
 
 if len(tags) !=1 and os.path.isdir('./copr-reporter'):
 
-    pages = ['f37', 'f38', 'f38-llvm18-20231016', 'f39', 'f38-llvm18-20231102']
+    pages = ['f37', 'f38', 'f38-llvm18-20240103', 'f39']
 
     print("COPR REPORTER", pages)
     old_cwd = os.getcwd()
